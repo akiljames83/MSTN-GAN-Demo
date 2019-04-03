365d05491801d77cd25e898f806b809b249ea853
Akil Hamilton <akil.james83@gmail.com>
2019-04-03 06:18:58 -0400

    Modify website style

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ image = document.querySelector("#image");
 prev = 0;
 cur = 0;
 
-function change_image() {
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function change_image() {
+	image.src = 'img/load_1.gif';
+
+	time = Math.floor(Math.random()*1500);
+	await sleep(1450);
+
 	while(cur == prev)
 		cur = Math.floor(1+Math.random() * 12);
+
 	prev = cur;
 	console.log(`img/img_${cur}.png`);
 	image.src = `img/img_${cur}.png`;
-	//alert("Changed");
+	
 }
